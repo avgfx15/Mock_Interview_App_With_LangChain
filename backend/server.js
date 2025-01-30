3c44daff2862df33973a6f953948c258d2715a4b
AVGfx <avgfx15@gmail.com>
2025-01-30 15:27:43 +0530

    Server and package.json update code and script

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,5 @@ app.get('/ping', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
